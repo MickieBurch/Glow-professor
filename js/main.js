3d9ad8b4e811ece1b99a913a2667adf9dc69b572
McKenna <Mckennaburch19@gmail.com>
2022-07-18 21:00:24 -0500

    CHANGED STYLING ECT

--- js/main.js
+++ js/main.js
@@ -76,37 +76,6 @@
     });
 
 
-    // Pricing carousel
-    $(".pricing-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        loop: true,
-        dots: false,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: true,
-        loop: true,
-        items: 1
-    });
-    
+     
 })(jQuery);
 
